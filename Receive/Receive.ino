da5120b19e65dcb57144e21cac94e7b3544831b6
Nicholas Chee-Awai <cheeordie1@gmail.com>
2016-12-24 13:54:21 -0600

    first commit

---
+++ Receive/Receive.ino
@@ -0,0 +1,31 @@
+#include <SPI.h>
+#include "VirtualWire.h"
+
+struct temp_pkg {
+  double internal;
+  double celsius;
+  double farenheit;
+};
+
+uint8_t transmit_pin = A0;
+temp_pkg heat_data;
+uint8_t data_len = sizeof(temp_pkg);
+uint16_t speed = 4000;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Setting up reception of heat data.");
+  vw_set_rx_pin(transmit_pin); // data output pin
+  vw_setup(speed); // bits per second
+  vw_rx_start();
+}
+
+void loop() {
+  if (vw_get_message((uint8_t *)&heat_data, &data_len))
+    {
+      Serial.println("Data received:");
+      Serial.println("Internal Temperature = " + String(heat_data.internal));
+      Serial.println(String(heat_data.celsius) + "C");
+      Serial.println(String(heat_data.farenheit) + "F");
+    }
+}
---
+++ Transmit/Transmit.ino
@@ -0,0 +1,38 @@
+#include <SPI.h>
+#include "Adafruit_MAX31855.h"
+#include "VirtualWire.h"
+
+uint8_t transmit_pin = 13;
+uint16_t speed = 4000;
+int seconds = 60;
+int thermoCLK = 10 ;
+int thermoCS = 9;
+int thermoDO = 8;
+
+int lpDelay(int);
+
+Adafruit_MAX31855 thermocouple(thermoCLK, thermoCS, thermoDO);
+
+struct temp_pkg {
+  double internal;
+  double celsius;
+  double farenheit;
+};
+
+void setup() {
+  vw_set_tx_pin(transmit_pin); // data output pin
+  vw_setup(speed); // bits per second
+}
+
+void loop() {
+  temp_pkg reading;
+  reading.internal = thermocouple.readInternal();
+  reading.celsius = thermocouple.readCelsius();
+  reading.farenheit = thermocouple.readFarenheit();
+  if (!isnan(reading.celsius) && !isnan(reading.farenheit))
+    {
+      vw_send((uint8_t *)&reading, sizeof(temp_pkg));
+      vw_wait_tx(); // wait until message sends
+    }
+  delay(1000); // wait 1 minute to send another message
+}
